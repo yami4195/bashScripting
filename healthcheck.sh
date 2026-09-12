3a8a7a4239f7ccc0acb7aa0494a672f71432ff20
YEAMLAK <yamlaksisay419@gmail.com>
2026-09-13 00:53:38 +0300

    implementation of DNS lookup

--- healthcheck.sh
+++ healthcheck.sh
@@ -6,5 +6,17 @@ echo "Checking $URL"
 
 WITH_OUT_PROTOCOL="${URL#*://}"
 HOST="${WITH_OUT_PROTOCOL%%/*}"
+
 echo "Url: $URL"
 echo "Hostname:$HOST"
+
+#DNS check
+
+IP=$(dig +short "$HOST" | head -n 1)
+
+if [ -n "$IP" ]; then
+echo "DNS: PASS - $IP"
+
+else
+echo "DNS: FAIL - could not resolve host name"
+fi
